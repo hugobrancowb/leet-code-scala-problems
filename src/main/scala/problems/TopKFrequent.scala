df5b79ec186ce8c7f0254f2753676a851d5afb9d
Hugo Branco <hugobrancowb@gmail.com>
2022-04-11 10:01:30 -0300

    :sparkles: TopKFrequent challenge (22-04-09)
    Signed-off-by: Hugo Branco <hugobrancowb@gmail.com>

---
+++ src/main/scala/problems/TopKFrequent.scala
@@ -0,0 +1,19 @@
+import scala.collection.mutable.HashMap
+
+object TopKFrequent {
+  def topKFrequent(nums: Array[Int], k: Int): Array[Int] = {
+    var frequencies = HashMap[Int, Int]()
+
+    for (n <- nums) {
+      var v: Int = frequencies.getOrElse(n, 0)
+      frequencies += n -> (v + 1)
+    }
+
+    frequencies.toArray
+      .sortWith(_._2 > _._2)
+      .map(t => t._1)
+      .slice(0, k)
+  }
+}
+
+// Aprendi HashMap, sort e getOrElse
