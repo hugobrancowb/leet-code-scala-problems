ad5d56e7a9040336ecec7c949ad5d9d8c113e069
Hugo Branco <hugobrancowb@gmail.com>
2022-04-11 10:01:55 -0300

    :sparkles: CalPoints challenge (22-04-10)
    Signed-off-by: Hugo Branco <hugobrancowb@gmail.com>

---
+++ src/main/scala/problems/CalPoints.scala
@@ -0,0 +1,19 @@
+object CalPoints {
+  def calPoints(ops: Array[String]): Int = {
+    var gameRecord = Array[Int]()
+
+    for (score <- ops) {
+      val s = gameRecord.length
+      score match {
+        case "+" => gameRecord = gameRecord :+ (gameRecord.apply(s - 2) + gameRecord.apply(s - 1))
+        case "D" => gameRecord = gameRecord :+ (gameRecord.apply(s - 1) * 2)
+        case "C" => gameRecord = gameRecord.dropRight(1)
+        case default => gameRecord = gameRecord :+ score.toInt
+      }
+    }
+
+    gameRecord.reduce((a, b) => a + b)
+  }
+}
+
+// Com esse desafio aprendi match (switch) e dropRight
